905c42c0512f627614e65bf2b5846ee39ba8b31b
angel <agavalda@xebialabs.com>
2018-08-21 14:15:44 +0200

    v3

--- XLRelease/Wassap/2.groovy
+++ XLRelease/Wassap/2.groovy
@@ -1,6 +1,6 @@
 // Exported from:        https://localhost:5516/xlr/#/templates/Folderfb3af7086a184b42b4f5b32973b440dc-Release17365cd320fd4d69b129e96734fdfd6c/releasefile
 // XL Release version:   8.2.0-rc.2
-// Date created:         Tue Aug 21 14:14:23 CEST 2018
+// Date created:         Tue Aug 21 14:15:43 CEST 2018
 
 xlr {
   template('2') {
@@ -10,6 +10,9 @@ xlr {
       phase('New Phase') {
         tasks {
           manual('2') {
+            
+          }
+          manual('4') {
             
           }
         }
