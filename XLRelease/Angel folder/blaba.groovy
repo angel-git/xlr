a2181662db2e185ce681574da583e5e2fb9d850d
angel <agavalda@xebialabs.com>
2018-08-22 11:01:11 +0200

    2

--- XLRelease/Angel folder/blaba.groovy
+++ XLRelease/Angel folder/blaba.groovy
@@ -1,6 +1,6 @@
 // Exported from:        https://localhost:5516/xlr/#/templates/Folder479287c16ad8427fbfe67d12cca929e9-Releaseb079c0234c5e4a80a33a9dc3a8f5bf16/releasefile
 // XL Release version:   8.2.0-rc.2
-// Date created:         Wed Aug 22 10:59:34 CEST 2018
+// Date created:         Wed Aug 22 11:01:10 CEST 2018
 
 xlr {
   template('blaba') {
@@ -10,6 +10,9 @@ xlr {
       phase('New Phase') {
         tasks {
           manual('1') {
+            
+          }
+          manual('2') {
             
           }
         }
